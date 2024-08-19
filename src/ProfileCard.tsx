e7d88d399c359d52efb3132a359213500e893e9b
deyanazhari1 <deyanazhari@gmail.com>
2024-08-19 11:11:16 +0700

    solve profileCard

---
+++ src/ProfileCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Profile {
+    bio:string;
+    website:string;
+  }
+  
+  const ProfileCard : React.FC<Profile> = ({bio, website}) => {
+    return (
+      <div>
+      <p>Bio : {bio}</p>
+      <p>Website : {website}</p>
+      </div>
+    )
+  }
+  export default ProfileCard;

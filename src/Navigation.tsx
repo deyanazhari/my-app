3e86f9e7f75e4ead9de27338e56aba74ec521e33
deyanazhari <deyanazhari@gmail.com>
2024-08-20 09:01:52 +0700

    new

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,39 +1,39 @@
 import React from 'react'
-import { Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
 import User from './User'
+// Latest version of useHistory, we are using react-router-dom v6, 
 import { useNavigate } from 'react-router-dom'
+import Profile from './Profile'
+
 const Navigation = () => {
     const navigate = useNavigate()
+
     const navigateProg = () => {
-        console.log("Do Something...")
-        //to simulate API CALL
-        navigate ('/user/123')
-        setTimeout(() => {
-
-        },500)
+        navigate('/user/123')
     }
-  return (
-<>
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/user/1">user1</Link></li>
-                <li><Link to="/user/2">user2</Link></li>
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/user/1">User 1</Link></li>
+                    <li><Link to="/user/2">User 2</Link></li>
+                    <li><Link to="/profile">Profile page</Link></li>
+                    <button onClick={navigateProg}>Navigate Programtically</button>
 
-
-            </ul>
-        </nav>
-        <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/about' element={<About/>}></Route>
-            <Route path='/user/:userId'  element={<User/>}></Route>
-
-        </Routes>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/about' element={<About />}></Route>
+                <Route path='/user/:userId' element={<User />}></Route>
+                <Route path='/profile/*' element={<Profile/>}></Route>
+            </Routes>
         </>
-  )
+    )
 }
 
 export default Navigation
